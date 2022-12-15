fa5d7ee573d7764ed494715351971ce73e98b5c0
Denisa Vilvoi <denisaionela.vilvoi@ulbsibiu.ro>
2022-12-15 12:37:56 +0200

    add home to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <template>
   <main>
-      <v-app>
+    <v-app>
 
-        <v-app-bar
-          color="orange"
-          elevation="20"
-          rounded 
-          title="NewsApp">
-          <v-btn href="/news" icon="">Search</v-btn>
-          <v-btn @click="toggleTheme">Theme</v-btn>
-          
-        </v-app-bar>
+      <v-app-bar color="orange" elevation="30" rounded title="NewsApp">
+     
+        <v-btn href="#/news" icon="mdi-magnify"></v-btn>
+        <v-btn href="#/" icon="mdi-home"></v-btn>
+        <v-btn @click="toggleTheme" icon="mdi-animation"></v-btn>
 
-        <v-main>
-          <component :is="currentView" />
-        </v-main>
+      </v-app-bar>
+      
+       
+      <v-main>
+        <component :is="currentView" />
+        <home v-if="(this.currentPath !== '#/news')" />
+      </v-main>
 
-      </v-app>
+    </v-app>
 
-    </main>
-  </template>
+  </main>
+</template>
     
-<script lang="ts">
-  import NewsApp from "./components/NewsApp.vue"
-  import { useTheme } from "vuetify";
+<script>
+import Search from "./components/Search.vue";
+import home from "./components/Home.vue";
+import { useTheme } from "vuetify";
 
-  
-  const routes = {
-  "/news": NewsApp,
+
+const routes = {
+  "/news": Search,
+  "/": home,
 };
 
 export default {
@@ -36,19 +38,20 @@ export default {
     return {
       theme,
       toggleTheme: () =>
-        (theme.global.name.value = theme.global.current.value.dark
-          ? "light"
-          : "dark"),
+      (theme.global.name.value = theme.global.current.value.dark
+        ? "light"
+        : "dark"),
     };
   },
   data() {
-    return {
-      currentPath: window.location.hash,
+    return { currentPath: window.location.hash,
+ 
+     
     };
   },
   computed: {
     currentView() {
-      return routes[this.currentPath.slice(1) || "/"] ;
+      return routes[this.currentPath.slice(1) || "/"];
     },
   },
   mounted() {
