597b4564554a9ec610b4486520d61aba84bb49f2
Denisa Vilvoi <denisaionela.vilvoi@ulbsibiu.ro>
2023-01-10 22:46:37 +0200

    start About page

---
+++ src/components/About.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container>
+    <v-carousel hide-delimiters>
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+</v-container>
+  </template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          src: './images/news.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  },
+}
+</script>
